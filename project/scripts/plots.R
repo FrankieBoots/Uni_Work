212be93e1eda0b961397488e92c8ebd2d69be710
FrankieBoots <104630169+FrankieBoots@users.noreply.github.com>
2024-03-04 18:55:22 +0000

    initial upload

---
+++ project/scripts/plots.R
@@ -0,0 +1,98 @@
+### Set up ####
+
+# working directory
+setwd("~/uni/Uni_Work/project")
+getwd()
+
+#libraries
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("readxl")
+library(readxl)
+
+install.packages("lubridate")
+library("lubridate")
+
+#Bring in raw data
+all <- read_excel("growth curves raw/Plate Reading GM LH CM JF.xlsx",
+                  sheet = "Absorbance 1_01", skip = 9)
+
+#########
+#### PROCESSING DATA ####
+#rename time column for all
+
+##LOUISE
+louise <- all %>% 
+  select("avg. time [s]","Un0001 (B01)","Un0002 (B02)","Un0003 (B03)","Un0004 (B04)","Un0005 (B05)") %>%
+  setNames(c("time_s", "one", "two","three","four","five"))
+#Get mean
+louise$mean <- rowMeans(louise[ , c(2,6)], na.rm=TRUE)
+#remove anything not in plot
+louise <- louise %>% select("time_s","mean")
+louise
+
+## CONNIE
+connie <- all %>% select("avg. time [s]","Un0006 (D01)","Un0007 (D02)","Un0008 (D03)","Un0009 (D04)","Un0010 (D05)") %>%
+  setNames(c("time_s", "one", "two","three","four","five"))
+#Get mean
+connie$mean <- rowMeans(connie[ , c(2,6)], na.rm=TRUE)
+#remove anything not in plot
+connie <- connie %>% select("time_s","mean")
+connie
+
+## GRACE
+grace <- all %>% select("avg. time [s]","Un0011 (F01)","Un0012 (F02)","Un0013 (F03)","Un0014 (F04)","Un0015 (F05)") %>%
+  setNames(c("time_s", "one", "two","three","four","five"))
+#Get mean
+grace$mean <- rowMeans(grace[ , c(2,6)], na.rm=TRUE)
+#remove anything not in plot
+grace <- grace %>% select("time_s","mean")
+grace
+
+## JAMES
+james <- all %>% select("avg. time [s]","Un0016 (H01)","Un0017 (H02)","Un0018 (H03)","Un0019 (H04)","Un0020 (H05)") %>%
+  setNames(c("time_s", "one", "two","three","four","five"))
+#Get mean
+james$mean <- rowMeans(james[ , c(2,6)], na.rm=TRUE)
+#remove anything not in plot
+james <- james %>% select("time_s","mean")
+james
+####
+
+#### Making the line plot ####
+
+
+
+## Individual ggplots ##
+#I have: james, grace, connie, louise
+
+#repeatable
+growth_curve <- louise
+
+# Make seconds into hours
+
+growth_curve <- growth_curve %>%
+  mutate(time_h = hour(seconds_to_period(time_s)))
+
+#plot
+single <- ggplot(growth_curve) +
+  geom_line(aes(x = time_s,
+                y = mean)) +
+  labs(y = "Mean Plate Reading",
+       x = "Time (S)") +
+  theme_classic()
+single
+
+# Fancy plot
+
+fancy <- ggplot(growth_curve) +
+  geom_line(aes(x = time_s,
+                y = mean)) +
+  labs(y = "Mean Plate Reading",
+       x = "Time (S)") +
+  theme_classic()
