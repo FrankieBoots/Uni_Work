5cf5161f2844b7c20d0f1aadf2728295631ffb38
FrankieBoots <104630169+FrankieBoots@users.noreply.github.com>
2024-03-09 17:40:41 +0000

    Making it much more repeatable

--- project/scripts/SDS Page weights.R
+++ project/scripts/SDS Page weights.R
@@ -4,16 +4,28 @@
 library(tidyverse)
 library(readxl)
 
+#function for me
+specify_decimal <- function(x, k) trimws(format(round(x, k), nsmall=k))
+
 ##Loading in data
+#Input data under columns 'distance' and 'mw_kDa'
 wb_ladder <- read_excel("project/Figuring out molecular weight.xlsx")
+
+#### my inputs ####
+# If input everything here, the rest should auto-run without any changes needed
+
+protein_name <- "PseB"
+solvent_front <- 8.08
+protein_distance <- 3.92
+plot_save_location <- "project/plots/"
 
 #### Finding linear regression coefficents ########
 
-#solvent front:8.08
+#solvent front:
 wb_ladder
 
 #finding the rf
-wb_ladder$rf <- (8.08-wb_ladder$distance)/8.08
+wb_ladder$rf <- (solvent_front-wb_ladder$distance)/solvent_front
 
 #getting log(molecular weight)
 wb_ladder$log_mw <- log10(wb_ladder$mw_kDa)
@@ -24,18 +36,40 @@ wb_ladder_lm <- lm(log_mw~rf, data=wb_ladder)
 
 #finding the equation
 
-summary(wb_ladder_lm)
+sum_wb_ladder_lm <- summary(wb_ladder_lm)
+sum_wb_ladder_coeff <- sum_wb_ladder_lm[["coefficients"]]
 
 # y = log(mw)
 # x = rf
-# y = 1.84828x + 2.44
+# y = mx + c
 
-mx <- 1.84828
-c<- 0.59180
-
+mx <- sum_wb_ladder_lm$coefficients["rf","Estimate"]
+c <- sum_wb_ladder_lm$coefficients["(Intercept)","Estimate"]
+r2 <- sum_wb_ladder_lm[["adj.r.squared"]]
 #R^2 = 0.9691
 
-line_equation <- "y = 1.84828x + 0.59180, R^2 = 0.97"
+line_equation <- paste("y = ",
+                       specify_decimal(mx,2),
+                       "x + ",specify_decimal(c,2),
+                       ", R^2 = ",
+                       specify_decimal(r2,2), sep="")
+line_equation
+
+#### Finding my MW for PseB ####
+
+#find rf
+
+rf <- (solvent_front-protein_distance)/solvent_front
+rf
+
+#find log(mw)
+log_Mw <- (mx*rf) + c
+log_Mw
+
+#mw
+PseB_Mw <- 10^(log_Mw)
+PseB_Mw
+
 
 #### plotting the calibration curve ####
 wb_ladder %>%
@@ -44,26 +78,15 @@ wb_ladder %>%
   geom_smooth(method = lm, se = F) +
   theme_classic() +
   annotate("text",x = 0.40, y=1, label = paste(line_equation), size = 4.5, hjust = 0) +
-  annotate("text",x = 0.40, y=2.2, label = paste("PseB = 35 kDa"), size = 4.5, hjust = 0)
+  annotate("text",x = 0.40, y=2.2, label = paste("PseB = 35 kDa"), size = 4.5, hjust = 0) +
   scale_y_continuous("log(Molecular Weight)") +
   scale_x_continuous("RF Value") +
   theme(axis.text=element_text(size=14),
         axis.title=element_text(size=14))
 
-ggsave("project/plots/wb_ladder_graph.png", width= 1800, height= 2000, units="px")
 
-#### Finding my MW for PseB ####
-
-#find rf
-
-rf <- (8.08-3.89)/8.08
-rf
-
-#find log(mw)
-log_Mw <- (1.84828*rf) + 0.59180
-log_Mw
-
-#mw
-PseB_Mw <- 10^(log_Mw)
-PseB_Mw
-#38.42061
+ggsave(paste(plot_save_location,protein_name,
+             "_SDSPage_Mw.png",sep = ""),
+       width= 1800,
+       height= 2000,
+       units="px")
